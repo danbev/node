938b85dfe478df366179cd9b5e278241223aae6f
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-06-01 21:35:57 +0200

    test: remove optional importObject argument
    This commit remove the optional importObject argument to
    WebAssembly.instantiate.

--- test/parallel/test-wasm-simple.js
+++ test/parallel/test-wasm-simple.js
@@ -10,7 +10,7 @@ const buffer = fixtures.readSync('test.wasm');
 
 assert.ok(WebAssembly.validate(buffer), 'Buffer should be valid WebAssembly');
 
-WebAssembly.instantiate(buffer, {}).then((results) => {
+WebAssembly.instantiate(buffer).then((results) => {
   assert.strictEqual(
     results.instance.exports.addTwo(10, 20),
     30,
