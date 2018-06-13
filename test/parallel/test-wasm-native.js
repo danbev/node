2d747add537174f4aece9e0f87c25d0d35b10c8b
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-06-13 15:24:18 +0200

    test: add sync compile test

--- test/parallel/test-wasm-native.js
+++ test/parallel/test-wasm-native.js
@@ -13,3 +13,8 @@ WebAssembly.instantiate(buffer).then((results) => {
   const fd = results.instance.exports.fopen();
   assert.strictEqual(fd, 22);
 });
+
+
+const m = new WebAssembly.Module(buffer);
+const instance = new WebAssembly.Instance(m);
+assert.strictEqual(instance.exports.fopen(), 22);
